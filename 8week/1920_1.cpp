6b9438c962829c28891c716266b575a840cba55c
uniye <92070609+uniye@users.noreply.github.com>
2023-06-22 19:29:55 +0900

    Add files via upload

---
+++ 8week/1920_1.cpp
@@ -0,0 +1,38 @@
+﻿#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+//이분탐색 - algorithm 사용, vector 사용
+
+int n, m;
+vector<int> vec;
+
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	cin >> n;
+	int a=0;
+	for (int i = 0; i < n; i++) {
+		cin >> a;
+		vec.push_back(a);
+	}
+	sort(vec.begin(),vec.end()); //이분탐색을 하기 위해 오름차순
+
+	cin >> m;
+	int num;
+	while(m--) {
+		cin >> num;
+		if (binary_search(vec.begin(), vec.end(), num)) cout << 1 << "\n";
+		else cout << 0 << "\n";
+	}
+
+	return 0;
+}
+
+
+
+
