e4304595bc0f889c634741b669dd924d05f5d50a
uniye <92070609+uniye@users.noreply.github.com>
2023-06-23 22:58:04 +0900

    Add files via upload

---
+++ 9week/1316.cpp
@@ -0,0 +1,32 @@
+﻿#include<iostream>
+#include<vector>
+#include<string>
+
+using namespace std;
+
+//아스키코드 이용
+//예전에 한 것도 첨부(bool을 이용하여)
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int N;
+  cin >> N;
+  int cnt = 0;
+  string s;
+  while (N--) {
+    cin >> s;
+    vector<int> vec(26, 0);
+    for (int i = 0; i < s.length(); i++)
+    {
+      if (vec[(int)s[i] - 97] == 0) vec[(int)s[i] - 97]++; //방문 안함
+      else if (s[i] != s[i - 1]) break; //방문했었는데 그전의 char과 다름
+      if(i==s.length()-1) cnt++; //break되지 않은 것들은 그룹단어
+    }
+  }
+  cout << cnt;
+
+  return 0;
+}
+
---
+++ 9week/1316_1.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+using namespace std;
+
+//bool타입 이용
+
+int main() {
+	int N;
+	cin >> N;
+	int sum = 0;
+	for (int i = 0; i < N; i++) {
+		bool check = true;
+		string word;
+		cin >> word;
+		int size = word.length();
+		for (int j = 0; j < size; j++)
+		{
+			for (int k = 0; k < j; k++)
+			{
+				if (word[j] != word[j - 1] && word[j] == word[k]) {
+					check = false;
+					break;
+				}
+				if (check == false) break;
+			}
+
+		}
+		if (check) ++sum;
+	}
+	cout << sum << "\n";
+	return 0;
