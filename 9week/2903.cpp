8d3c2a78d6dc002fd70f88eaae4353f9cb08efef
uniye <92070609+uniye@users.noreply.github.com>
2023-06-24 16:44:04 +0900

    Add files via upload

---
+++ 9week/2903.cpp
@@ -0,0 +1,24 @@
+﻿#include<iostream>
+#include<vector>
+
+using namespace std;
+
+vector<int> vec(16, 0);
+
+// 규칙찾음 => 규칙 사진으로 정리
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int j=1, k=2, N;
+  cin >> N;
+  for (int i = 0; i <= 15; i++)
+  {
+    vec[i] = k*k;
+    k += j;
+    j *= 2;
+  }
+  cout << vec[N];
+
+  return 0;
+}
