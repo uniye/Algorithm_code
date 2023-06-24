13b759c14e410a41c8e1171ae11f22e128187b45
uniye <92070609+uniye@users.noreply.github.com>
2023-06-25 00:11:07 +0900

    Add files via upload

---
+++ 9week/10798.cpp
@@ -0,0 +1,31 @@
+﻿#include<iostream>
+#include<string>
+#include<vector>
+
+using namespace std;
+
+vector<string> vec(15,""); //배열 초기화 중요
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  
+  int N = 5;
+  string s;
+  for (int i = 0; i < N; i++)
+  {
+    cin >> s;
+    for (int j = 0; j < s.length(); j++)
+    {
+      vec[j] += s[j];
+    }
+  }
+  
+  for (int i = 0; i < vec.size(); i++)
+  {
+    cout << vec[i];
+  }
+
+
+  return 0;
+}
