5d736e7e4d85595e51a602ab88ad421ea24fbff4
uniye <92070609+uniye@users.noreply.github.com>
2023-06-25 13:53:50 +0900

    Add files via upload

---
+++ 9week/2941.cpp
@@ -0,0 +1,29 @@
+﻿#include<iostream>
+#include<string>
+#include<vector>
+
+using namespace std;
+
+vector<string> croatia = { "c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z=" };
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  string s;
+  int result;
+  cin >> s;
+
+  for (int i = 0; i < croatia.size(); i++) {
+    while (true) {
+      int index = s.find(croatia[i]);
+      if (index == string::npos) { //없는 경우
+        break;
+      }
+      s.replace(index, croatia[i].length(), "!"); //크로아티아 문자 차례대로 없애버리기
+    }
+  }
+  result = s.length();
+  cout << result << "\n";
+  return 0;
+}
