4fa70cde1cc1534f87e5a4d899a8f21041aa0367
uniye <92070609+uniye@users.noreply.github.com>
2023-06-25 13:19:18 +0900

    Add files via upload

---
+++ 9week/10814.cpp
@@ -0,0 +1,42 @@
+﻿#include<iostream>
+#include<string>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+vector<pair<int, string>> vec;
+
+// 처음에 vector 인덱스 0~199까지 나이로 하려 했는데 
+// 어떻게 구현할지 모르겠어서
+// 일단은 pair을 사용
+// stable_sort와 sort에 대한 차이점 => 10814.md 참고
+
+
+bool compair(pair<int, string> age, pair<int, string> name){
+  return age.first < name.first;
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int N, age;
+  string name;
+  cin >> N;
+
+  while (N--) {
+    cin >> age >> name;
+    vec.push_back(make_pair(age, name));
+  }
+
+  stable_sort(vec.begin(), vec.end(), compair);
+
+  for (int i = 0; i < vec.size(); i++)
+  {
+    cout << vec[i].first << " " << vec[i].second << "\n";
+  }
+
+  return 0;
+}
+
---
+++ 9week/10814_1.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// 구현 완료! vector속 vector 이용! 
+// emplace_back과 push_back의 차이에 대해서도 10814.md에 작성해 두도록 하겠다.
+// 훨씬 단축된 시간과 메모리!
+
+
+vector<vector<string>> vec(201);
+
+int main()
+{
+	ios_base::sync_with_stdio(false); 
+	cin.tie(nullptr);
+
+	int N;
+	cin >> N;
+
+	for (int i = 0; i < N; i++)
+	{
+		int num;
+		string name;
+
+		cin >> num >> name;
+
+		vec[num].emplace_back(name);
+	}
+
+	for (int i = 0; i < 201; i++)
+	{
+		for (int j = 0; j < vec[i].size(); j++)
+		{
+			cout << i << " " << vec[i][j] << "\n";
+		}
+	}
+
+	return 0;
+}
