c31fa3d838830334dbe4e332a89cdfdaf00f0468
uniye <92070609+uniye@users.noreply.github.com>
2023-06-27 21:18:08 +0900

    Add files via upload

---
+++ 9week/1764.cpp
@@ -0,0 +1,37 @@
+﻿#include<iostream>
+#include<string>
+#include<map>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+map<string, int> m; //이름이 key 언급되는 횟수가 value
+vector<string> vec;
+
+// find는 쓰지 않도록 하자. => 시간초과가 그 이유
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int N, M;
+  cin >> N >> M;
+
+  string name;
+  for (int i = 0; i < N+M; i++) {
+    cin >> name;
+    m[name]++;
+    if (m[name] > 1)
+      vec.push_back(name);
+  }
+  sort(vec.begin(),vec.end());
+  cout << vec.size() << "\n";
+  for (int i = 0; i < vec.size(); i++)
+  {
+    cout << vec[i] << '\n';
+  }
+
+
+  return 0;
+}
---
+++ 9week/1764_1.cpp
@@ -0,0 +1,38 @@
+﻿#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+vector<string>vec, vec1;
+
+//이분탐색 사용 => 시간 단축의 결과
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  
+  int N,M;
+  string s;
+  cin >> N >> M;
+  for (int i = 0; i < N; i++) {
+    cin >> s;
+    vec.push_back(s);
+  }
+  sort(vec.begin(), vec.end());
+
+  for (int i = 0; i < M; i++) {
+    cin >> s;
+    if (binary_search(vec.begin(), vec.end(), s)) {
+      vec1.push_back(s);
+    }
+  }
+  sort(vec1.begin(), vec1.end());
+
+  cout << vec1.size() << "\n";
+  for (int i = 0; i < vec1.size(); i++)
+  {
+    cout << vec1[i] << "\n";
+  }
+}
