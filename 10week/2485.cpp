2a995b9975469cf4bab3a18074ea6db4575e826b
uniye <92070609+uniye@users.noreply.github.com>
2023-07-04 21:43:41 +0900

    Add files via upload

---
+++ 10week/2485.cpp
@@ -0,0 +1,51 @@
+﻿#include<iostream>
+#include<vector>
+#include <algorithm>
+
+using namespace std;
+
+// 최대공약수가 간격 맞춰주는 애.
+// (가로수들 간격 / 최대공약수) -1 을 모두 더한 것이 정답
+
+vector<int> vec;
+vector<int> dis(1000000,0);
+
+int Gcd(int a, int b) {
+  int r = a % b;
+  if (r == 0) return b;
+  else return Gcd(b, r);
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int N, gcd, cnt=0, tree;
+  cin >> N;
+
+  for (int i = 0; i < N; i++)
+  {
+    cin >> tree;
+    vec.emplace_back(tree);
+  }
+  sort(vec.begin(), vec.end());
+
+  for (int i = 0; i < N - 1; i++) {
+    dis[i] = vec[i + 1] - vec[i];
+  }
+
+
+  gcd = dis[0];
+  for (int i = 0; i < N-1; i++)
+  {
+    gcd = Gcd(gcd, dis[i]);
+  }
+
+  for (int i = 0; i < N - 1; i++) {
+    cnt += (dis[i] / gcd) - 1;
+  }
+
+  cout << cnt;
+
+  return 0;
+}
---
+++ 10week/4134.cpp
@@ -0,0 +1,33 @@
+﻿#include<iostream>
+#include<cmath>
+
+using namespace std;
+
+// O(N) 알고리즘은 시간 초과가 걸림
+// 제곱근을 이용하여 문제 풀기!: 
+// 어떤 자연수 n이 소수가 아니라면 n은 p * q의 형태로 나타낼 수 있다. 
+// 이때 p와 q 중 하나는 반드시 sqrt(n)(제곱근) 이하의 값을 가지고, 다른 하나는 sqrt(n) 이상의 값을 가진다.
+
+long long multiple(long long n) {
+  if (n == 0 || n == 1) return 2;	
+  for (int i = 2; i <= sqrt(n); i++) {
+    if (n % i == 0) return multiple(n + 1);
+  }
+  return n;
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  long long n, a;
+  cin >> n;
+
+  for (long long i = 0; i < n; i++) {
+    cin >> a;
+    long long ans = multiple(a);
+    cout << ans << "\n";
+  }
+
+  return 0;
+}
