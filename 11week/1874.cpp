c21beafd3f6d3c2e0fdbfc2f00d6f66839b32205
uniye <92070609+uniye@users.noreply.github.com>
2023-07-09 21:09:51 +0900

    Add files via upload

---
+++ 11week/1874.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <stack>
+#include <vector>
+
+using namespace std;
+
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int cnt = 1;
+	int n;
+	cin >> n;
+
+	vector<char> result;
+	stack<int> s; 
+
+	for (int i = 0; i < n; i++)
+	{
+		int x;
+		cin >> x;
+
+		while (cnt <= x)
+		{
+			s.push(cnt);
+			cnt += 1;
+			result.emplace_back('+');
+		}
+
+		if (s.top() == x)
+		{
+			s.pop();
+			result.emplace_back('-');
+		}
+		else
+		{
+			cout << "NO";
+			return 0;
+		}
+	}
+
+	for (int i = 0; i < result.size(); i++)
+	{
+		cout << result[i] << '\n';
+	}
+}
---
+++ 11week/9012.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <string>
+#include <stack>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int T;
+	cin >> T;
+	for (int i = 0; i < T; i++)
+	{
+		string an = "YES";
+		stack<char> s1;
+		string input;
+		cin >> input;
+		for (int i = 0; i < input.length(); i++) {
+			if (input[i] == '(') {
+				s1.push(input[i]);
+			}
+			else if (!s1.empty() && input[i] == ')' && s1.top() == '(') {
+				s1.pop();
+			}
+			else {
+				an = "NO";
+			}
+
+		}
+		if (!s1.empty()) {
+			an = "NO";
+		}
+
+		cout << an << "\n";
+	}
+
+	return 0;
+}
