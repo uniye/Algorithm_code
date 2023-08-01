ecbae16612824d701ad654088038f45e36aef706
uniye <92070609+uniye@users.noreply.github.com>
2023-08-01 18:47:52 +0900

    Add files via upload

---
+++ 14week/SW_2047.cpp
@@ -0,0 +1,17 @@
+﻿#include<iostream>
+#include<string>
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    string sInput;
+    cin >> sInput;
+    for (int i = 0; i < sInput.size(); i++)
+    {
+        if ((sInput[i] < 97) || (sInput[i] > 122))
+            continue;
+        sInput[i] -= 32;
+    }
+    cout << sInput;
+    return 0;//정상종료시 반드시 0을 리턴해야합니다.
+}
