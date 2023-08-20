668cbd02f304f38bfa91eff17ca871adbc06b4af
uniye <92070609+uniye@users.noreply.github.com>
2023-08-20 15:58:56 +0900

    Add files via upload

---
+++ 16week/11729.cpp
@@ -0,0 +1,29 @@
+﻿#include<iostream>
+
+using namespace std;
+
+void hanoi(int _iN, int _iStart, int _iTo, int _iPass);
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int iNum;
+    cin >> iNum;
+    cout << (1 << iNum) - 1 << "\n";
+    hanoi(iNum, 1, 3, 2);
+    return 0;
+}
+
+void hanoi(int _iN, int _iStart, int _iTo, int _iPass)
+{
+    if (_iN == 1)
+        cout << _iStart << ' ' << _iTo << '\n';
+    else
+    {
+        hanoi(_iN - 1, _iStart, _iPass, _iTo);
+        cout << _iStart << ' ' << _iTo << '\n';
+        hanoi(_iN - 1, _iPass, _iTo, _iStart);
+    }
+}
+
---
+++ 16week/20920.cpp
@@ -0,0 +1,48 @@
+﻿#include<iostream>
+#include<string>
+#include<map>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+bool cmp(pair<string, int>& a, pair<string, int>& b);
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int iN(0), iM(0);
+    string st;
+    map<string, int> m1;
+
+    cin >> iN >> iM;
+
+    for (int i = 0; i < iN; i++)
+    {
+        cin >> st;
+        if (st.size() >= iM)
+            m1[st]++;
+    }
+
+    vector<pair<string, int>> vec(m1.begin(), m1.end());
+    sort(vec.begin(), vec.end(), cmp);
+
+    for (auto p : vec)
+        cout << p.first << "\n";
+
+    return 0;
+}
+
+bool cmp(pair<string, int>& a, pair<string, int>& b)
+{
+    if (a.second == b.second)
+    {
+        if (a.first.length() == b.first.length())
+            return a.first < b.first;
+        return a.first.length() > b.first.length();
+    }
+    else return a.second > b.second;
+}
+
+
