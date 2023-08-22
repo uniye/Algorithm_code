28e429655930655679ee4d52c60d7d841ea0fe27
uniye <92070609+uniye@users.noreply.github.com>
2023-08-22 11:40:52 +0900

    Add files via upload

---
+++ 16week/P136798.cpp
@@ -0,0 +1,44 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int number, int limit, int power) {
+    int answer = 0;
+    for (int i = 1; i <= number; i++)
+    {
+        int iDem(1);
+        for (int j = 1; j <= i / 2; j++)
+        {
+            if (i % j == 0) iDem++;
+        }
+        if (iDem > limit) iDem = power;
+        answer += iDem;
+    }
+
+    return answer;
+}
+
+
+//시간초과 - 실패
+//#include <string>
+//#include <vector>
+//
+//using namespace std;
+//
+//int solution(int number, int limit, int power) {
+//    int answer = 0;
+//    for (int i = 1; i <= number; i++)
+//    {
+//        int iDem(0);
+//        for (int j = 1; j <= number; j++)
+//        {
+//            if (i % j == 0) iDem++;
+//        }
+//        if (iDem > limit) iDem = power;
+//        answer += iDem;
+//    }
+//
+//    return answer;
+//}
+
---
+++ 16week/S1954.cpp
@@ -0,0 +1,59 @@
+﻿#include<iostream>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	int test_case;
+	int T, iN;
+
+	cin >> T;
+
+	for (test_case = 1; test_case <= T; ++test_case)
+	{
+		int iArr[11][11] = { 0, };
+		int iRow = 0, iCol = -1;
+		int iK(1), iDir(1), iNum(0);
+		cin >> iN;
+		iNum = iN;
+
+		while (iN > 0)
+		{
+			for (int i = 0; i < iN; i++)
+			{
+				iCol += iDir;
+				iArr[iRow][iCol] = iK;
+
+				iK++;
+			}
+
+			iN--;
+
+			for (int j = 0; j < iN; j++)
+			{
+				iRow += iDir;
+				iArr[iRow][iCol] = iK;
+
+				iK++;
+			}
+
+			iDir *= -1;
+		}
+
+		cout << "#" << test_case << endl;
+
+		for (int i = 0; i < iNum; i++) {
+			for (int j = 0; j < iNum; j++) {
+				cout << iArr[i][j] << " ";
+			}
+			cout << endl;
+
+		}
+
+
+
+	}
+	return 0;
+}
+
+
